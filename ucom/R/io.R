a893dbedfe1fc90778091772a3aedf5aa5c76b60
Xinbin Huang <xbhuang93@hotmail.com>
2019-01-30 23:22:36 -0800

    Move function read_cleaned_data to ucom

--- ucom/R/io.R
+++ ucom/R/io.R
@@ -1,9 +1,9 @@
 #' Input raw data
 #'
-#' @param site
-#' @param type
-#' @param start_row
-#' @param file_name
+#' @param site Data folder (site) name
+#' @param type Numeric or Choice value data
+#' @param start_row number of rows to skip meta-data (description on variables)
+#' @param file_name If provided, will read in this file rather than figure out itself
 #'
 #' @return
 #' @export
@@ -26,11 +26,32 @@ get_raw_data <- function(site, type, start_row=3, file_name=NULL) {
       return(df)
 }
 
+#' Read in cleaned data for merging
+#'
+#' @param df_path Cleaned data path
+#' @param num_vars_path Path to the related num_vars.json
+#' @param other_vars Other variables to include except for numeric
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_cleaned_data <- function(df_path,
+                              num_vars_path,
+                              other_vars) {
+      df <- read_csv(df_path, col_types = cols())
+      num_vars <- read_rds(num_vars_path)
+
+      all_vars <- append(other_vars, num_vars)
+      out <- df %>% select(all_vars)
+      return(out)
+}
+
 #' Write the result: variable .rds file
 #'
-#' @param path
-#' @param all_vars
-#' @param num_vars
+#' @param path path to output num_vars.rds and non_num_vars.rds
+#' @param all_vars a vector of all variables
+#' @param num_vars a vector of numeric variables
 #'
 #' @return
 #' @export
@@ -48,11 +69,11 @@ write_vars_rds <- function(path,
 
 #' Write the result: data .csv file
 #'
-#' @param choice_df
-#' @param all_vars
-#' @param num_vars
-#' @param country_code
-#' @param output_path
+#' @param choice_df Choice data frame or transformed one if necessary
+#' @param all_vars a vector of all variables
+#' @param num_vars a vector of numeric variables
+#' @param country_code Default NULL, if provided, will use the provided one instead of
+#'      figuring out itself.
 #'
 #' @return
 #' @export
@@ -61,16 +82,15 @@ write_vars_rds <- function(path,
 write_results <- function(choice_df,
                           all_vars,
                           num_vars,
-                          country_code=NULL,
-                          output_path=NULL) {
+                          country_code=NULL) {
       # get the path
       site <- get_current_site()
 
-      if (is_null(output_path)) {
-            output_path <- rprojroot::find_root_file('cleaned_data',
-                                                     site,
-                                                     criterion = rprojroot::has_dir('.git'))
-      }
+
+      output_path <- rprojroot::find_root_file('cleaned_data',
+                                               site,
+                                               criterion = rprojroot::has_dir('.git'))
+
 
       table_name <- str_glue('{stringr::str_to_lower(site)}.csv')
       message('Output path: ', output_path)
@@ -80,17 +100,14 @@ write_results <- function(choice_df,
       # Convert data frame to remove unncessary text
       out_df <- choice_df %>% convert_choiceDF(num_vars)
 
-
+      # create country code & site
+      country_collector <- stringr::str_split(site, '_', n=2) %>% unlist()
+      country <- country_collector[1]
 
       # if country code not provided
       if (is_null(country_code)) {
             if (country %in% country_codes$Country) {
-                  # create country code & site
-                  country_collector <- stringr::str_split(site, '_', n=2) %>% unlist()
-                  country <- country_collector[1]
-
                   country_code <- get_country_code(country)
-
                   out_df <- ucom::create_country_and_site(out_df,
                                                           country_code,
                                                           site)
