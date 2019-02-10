690b852a1be4bb30b24f2593cdb93585d3751e6e
Xinbin Huang <xbhuang93@hotmail.com>
2019-02-10 15:15:46 -0800

    Add 2 more sites: Nethelands Otten & Ireland Bosak

---
+++ src/sites-spec/Ireland_Bosak/integrity_check.R
@@ -0,0 +1,53 @@
+#!/usr/bin/env Rscript
+
+################ Set up #####################
+suppressMessages(library(ucom))
+
+################ Loading Data #####################
+
+message('\n\n')
+script_path <- get_rel_path(rprojroot::thisfile())
+message('Script: ', script_path)
+message('===== Loading data =====')
+site <- get_current_site()
+
+message("Only has one SAV file")
+message("raw_data/Ireland_Bosak/UCOM data_Ireland_FINAL_19.11.2018merged.sav")
+message('Rows dropped: 0')
+
+choice_df <- haven::read_sav(file.path('raw_data',
+                                       site, 'UCOM data_Ireland_FINAL_19.11.2018merged.sav'))
+
+message('======= Create duration_seconds, startdate and enddate =======')
+message('Rename `Location` to `uni`....')
+choice_df <- choice_df %>% convert_spss() %>%
+      dplyr::mutate(duration_seconds = NA_character_,
+                    startdate = NA_character_,
+                    enddate = NA_character_) %>%
+      dplyr::rename(uni = Location) %>%
+      convert_names()
+################ Checking #####################
+
+message('======= Filtering out error entries =======')
+choice_df <-
+      choice_df %>% filter(
+            parentleave_motivati_1 <= 100 | is.na(parentleave_motivati_1),
+            parentleave_efficacy != '54' | is.na(parentleave_efficacy),
+            real_pay_2 <= 100 | is.na(real_pay_2),
+            other_child_3 != '47' | is.na(other_child_3)
+      )
+
+message('===== Checking =====')
+all_vars <- colnames(choice_df)
+
+num_vars <- all_vars %>%
+      get_num_vars('(^q[0-9]+)|(text)|(filter_\\$)|(major_double)|(number)|(area)|(class)')
+
+message('No check for this! Only one data set')
+################ Write out results #####################
+
+message('===== Writing results =====')
+ucom::write_results(choice_df, all_vars, num_vars)
+message('Sucessfully write results!')
+
+
--- src/sites-spec/Netherlands_Otten/integrity_check.R
+++ src/sites-spec/Netherlands_Otten/integrity_check.R
@@ -20,6 +20,45 @@ choice_df <- haven::read_sav(file.path('raw_data',
 
 choice_df <- choice_df %>% convert_spss() %>% convert_names()
 
+message('=========== Fixing variable names !! ===========')
+colnames(choice_df) <- colnames(choice_df) %>%
+      stringr::str_replace_all(
+            c(
+                  '^a__(.*)$' = '\\1',
+                  'communal_values__([123])' = 'values_\\1',
+                  'power_values__1' = 'values_4',
+                  'power_values__2' = 'values_5',
+                  'power_values__3' = 'values_6',
+                  'achievement_values__1' = 'values_7',
+                  'achievement_values__2' = 'values_8',
+                  'achievement_values__3' = 'values_9',
+                  'exp_domestic4_1' = 'react_domestic_4',
+                  'react_work3_4_1' = 'react_work_3',
+                  'react_work3_4_2' = 'react_work_4',
+                  'react_work5_1' = 'react_work_5',
+                  'react_work5_2' = 'react_work_6',
+                  'own_support6_1' = 'own_support_6',
+                  'own_support6_2' = 'own_support_7',
+                  'exp_conflict_1' = 'exp_conflict',
+                  'religiosity_1' = 'religiosity',
+                  'political_1' = 'political',
+                  'parentleave_expectat_1' = 'parentleave_efficacy',
+                  'expected_parental_le' = 'parentleave_expectat',
+                  'ses_1' = 'ses',
+                  '^([a-z_]+)([0-9])_1$' = '\\1_\\2',
+                  '^(.*)_1\\.0$' = '\\1_4',
+                  '^(.*)_2\\.0$' = '\\1_5',
+                  '^(.*)_3\\.0$' = '\\1_6',
+                  '^([a-z]+(_[a-z]+)?)_?1_2(_3)?_([123])' = '\\1_\\4',
+                  '^(.*)4_5_6_1' = '\\1_4',
+                  '^(.*)4_5_6_2' = '\\1_5',
+                  '^(.*)4_5_6_3' = '\\1_6',
+                  '^(.*)7_8_9_1' = '\\1_7',
+                  '^(.*)7_8_9_2' = '\\1_8',
+                  '^(.*)7_8_9_3' = '\\1_9'
+            )
+      )
+
 ################ Checking #####################
 
 message('===== Checking =====')
@@ -28,9 +67,7 @@ all_vars <- colnames(choice_df)
 num_vars <- all_vars %>%
       get_num_vars('(^q[0-9]+)|(text)|(sona_id)')
 
-converted_choice_df <- convert_choiceDF(choice_df, num_vars)
-check_vars(numeric_df, converted_choice_df, num_vars)
-message('Checked: Passed!')
+message('No check for this! Only one data set')
 ################ Write out results #####################
 
 message('===== Writing results =====')
