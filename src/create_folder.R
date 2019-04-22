b2c31765972ffbd204e500cde99888cc5aade543
Xinbin Huang <xbhuang93@hotmail.com>
2019-04-21 17:07:37 -0700

    Automate clean up outdated folders.

--- src/create_folder.R
+++ src/create_folder.R
@@ -2,5 +2,13 @@
 
 ### Create folders based on the folders in root/raw_data
 
-dir_list <- basename(fs::dir_ls(here::here('raw_data')))
-ucom::create_src_folders(dir_list)
+data_list <- basename(fs::dir_ls(here::here('raw_data')))
+src_path <- here::here('src', 'sites-spec')
+src_list <- basename(fs::dir_ls(src_path))
+
+message('Delete outdated src folders')
+del_dir <- file.path(src_path, setdiff(src_list, data_list))
+fs::dir_delete(del_dir)
+
+message('Create src folders')
+ucom::create_src_folders(data_list)
