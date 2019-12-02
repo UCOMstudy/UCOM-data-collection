2a804d15d4fc05f628c12e9f7dbd0c8afeeeb2ca
Xinbin Huang <xbhuang93@hotmail.com>
2019-12-02 13:23:01 -0800

    Fix Nertherland Otten issues

--- src/sites-spec/Netherlands_Otten/integrity_check.R
+++ src/sites-spec/Netherlands_Otten/integrity_check.R
@@ -23,12 +23,12 @@ numeric_df <- purrr::map_dfr(numeric_files,
 choice_df <- purrr::map_dfr(choice_files,
                             ~ get_raw_data(site, 'Choice',
                                            file_name = .x)) %>% convert_names()
-stop()
+
 message('=========== Fixing variable names !! ===========')
 colnames(choice_df) <- colnames(choice_df) %>%
+      stringr::str_replace_all(c('\'|[:space:]|,' = '_')) %>%
       stringr::str_replace_all(
             c(
-                  '^a__(.*)$' = '\\1',
                   'communal_values__([123])' = 'values_\\1',
                   'power_values__1' = 'values_4',
                   'power_values__2' = 'values_5',
@@ -36,6 +36,7 @@ colnames(choice_df) <- colnames(choice_df) %>%
                   'achievement_values__1' = 'values_7',
                   'achievement_values__2' = 'values_8',
                   'achievement_values__3' = 'values_9',
+                  'react_domestic3_1' = 'react_domestic_3',
                   'exp_domestic4_1' = 'react_domestic_4',
                   'react_work3_4_1' = 'react_work_3',
                   'react_work3_4_2' = 'react_work_4',
@@ -49,10 +50,8 @@ colnames(choice_df) <- colnames(choice_df) %>%
                   'parentleave_expectat_1' = 'parentleave_efficacy',
                   'expected_parental_le' = 'parentleave_expectat',
                   'ses_1' = 'ses',
+                  '\\*_own_parentalleave1_1' = 'own_parentalleave_1',
                   '^([a-z_]+)([0-9])_1$' = '\\1_\\2',
-                  '^(.*)_1\\.0$' = '\\1_4',
-                  '^(.*)_2\\.0$' = '\\1_5',
-                  '^(.*)_3\\.0$' = '\\1_6',
                   '^([a-z]+(_[a-z]+)?)_?1_2(_3)?_([123])' = '\\1_\\4',
                   '^(.*)4_5_6_1' = '\\1_4',
                   '^(.*)4_5_6_2' = '\\1_5',
@@ -69,7 +68,7 @@ message('===== Checking =====')
 all_vars <- colnames(choice_df)
 
 num_vars <- all_vars %>%
-      get_num_vars('(^q[0-9]+)|(text)|(sona_id)')
+      get_num_vars('(^q[0-9]+)|(text)|(sona-id)|(comments)')
 
 message('No check for this! Only one data set')
 ################ Write out results #####################
